1a46fa0d9cc65f9aeff4d1830b10ebd2a93780c2
Geekosaure <juliette.destang@epitech.eu>
2024-07-23 19:48:49 +0900

    [STEP 2] Affichage des 4 notifications dans une page

--- homeys-test-front-master/src/App.vue
+++ homeys-test-front-master/src/App.vue
@@ -2,6 +2,9 @@
   <main>
     <!-- Example d'utilisation du composant BaseNotification -->
     <BaseNotification title="Modal Window" message="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor" type="danger" />
+    <BaseNotification title="Success!" message="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor" type="success" />
+    <BaseNotification title="Warning" message="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor" type="warning" />
+    <BaseNotification title="This is an info" message="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor" type="info" />
   </main>
 </template>
 
