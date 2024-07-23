0473ab800f9329b3cd2c2e0e9145173f738b7d3b
Geekosaure <juliette.destang@epitech.eu>
2024-07-23 19:46:55 +0900

    [STEP 1] Affichage d'une notification "Warning"

---
+++ homeys-test-front-master/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <!-- Example d'utilisation du composant BaseNotification -->
+    <BaseNotification title="Modal Window" message="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor" type="danger" />
+  </main>
+</template>
+
+<script>
+import BaseNotification from './components/BaseNotification.vue';
+
+export default {
+  components: {
+    BaseNotification
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ homeys-test-front-master/src/components/BaseNotification.vue
@@ -0,0 +1,49 @@
+<!-- COMPOSANT NOTIFICATION -->
+<template>
+  <div class="notification" :class="notificationClass">
+    <img :src="iconSrc" alt="Description de l'image">
+    <div class="content">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+  </div>
+    <img src="/icon/Cross.svg" alt="Description de l'image" class="close">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseNotification',
+  props: {
+    title: String,
+    message: String,
+    type: {
+      type: String,
+      default: 'info',
+      // Les 4 types valides de notifications
+      validator: function(value) {
+        return ['warning', 'danger', 'success', 'info'].includes(value);
+      }
+    }
+  },
+  computed: {
+    //Permet d'obtenir un nom de class en fonction du type
+    notificationClass() {
+      return `notification-${this.type}`;
+    },
+    iconSrc() {
+      // Map liant type de notification à l'icon associé
+      const iconMap = {
+        danger: '/icon/Danger.svg',
+        info: '/icon/Info.svg',
+        success: '/icon/CheckCircle.svg',
+        warning: '/icon/Warning.svg'
+      };
+      return iconMap[this.type]
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import './BaseNotification.css';
+</style>
